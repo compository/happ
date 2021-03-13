dc30d44f987e403d726b87884f95549641caa18a
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-03-13 15:40:46 +0100

    Fixed release

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,9 @@
-FROM guillemcordoba/rsm:97bfc6c
+FROM guillemcordoba/rsm:d3a6144
 
 RUN apt-get update && apt-get install curl -y
 RUN curl -sL https://deb.nodesource.com/setup_12.x | bash
-RUN apt-get install -y nodejs
+RUN apt-get install -y python3
 
-RUN npm i -g @web/dev-server
 RUN apt-get install -y unzip
 
 RUN mkdir /happ
--- execute.sh
+++ execute.sh
@@ -5,14 +5,22 @@
 socat tcp-l:22222,fork,reuseaddr tcp:127.0.0.1:22202 & 
 socat tcp-l:22223,fork,reuseaddr tcp:127.0.0.1:22203 & 
 
-sleep 2 && wds --root-dir /app --port 8888 &
+python3 -m http.server -d /app 8888 &
+
+cd /database
 
 ### Check if a directory does not exist ###
-if [ ! -d "/database/sandbox" ] 
+if [ ! -d "/database/done" ] 
 then
-    hc sandbox create --root /database -d=sandbox network --bootstrap https://bootstrap-staging.holo.host/ quic -p=kitsune-proxy://sINaxH2sL-n4HminpxHEox5flT-ve_tzMK8NtJw3Fck/kitsune-quic/h/3.141.223.68/p/5778/--
+    # This is the first run: create sandbox and run
+    hc sandbox create --root /database -d=sandbox network --bootstrap https://bootstrap-staging.holo.host/ quic -p=kitsune-proxy://LotlbxvtyfXEsR9Hv1Xpza-AvafacWTLvW9Cj1cg5OI/kitsune-quic/h/3.141.223.68/p/22224/--
     hc sandbox call install-app-bundle /happ/compository.happ
-    hc sandbox -f=22202 run ---ports=22203
+    mkdir /database/done
+    hc sandbox -f=22202 run --ports=22203
 else
-    hc sandbox -f=22202 run
+    # This is the second run: reset keystore and run
+    rm -rf /database/sandbox/keystore
+    lair-keystore -d /database/sandbox/keystore &
+    sleep 3
+    holochain -c /database/sandbox/conductor-config.yaml
 fi
